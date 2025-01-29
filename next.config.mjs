50494986e0bd931c8436b501cf573d6ffbc141ca
bright5558 <132391017+bright5558@users.noreply.github.com>
2025-01-29 21:01:31 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,13 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
   eslint: {
     ignoreDuringBuilds: true,
   },
